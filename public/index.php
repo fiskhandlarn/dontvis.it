200240fb139411a19bfb627283459e7a201e9c48
Oskar Joelson <oskar@joelson.org>
2020-01-25 21:15:49 +0100

    Remove example input (from bootstrap docs(?)) :O

--- public/index.php
+++ public/index.php
@@ -68,7 +68,6 @@ require __DIR__ . '/../vendor/autoload.php';
                     <div class="col-md-8" id="theInputForm">
                         <form class="form-inline" id="uv-form">
                             <div class="form-group">
-                                <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><a href="<?php echo ROOT_URL; ?>/" id="logo" ><strong><?php echo env('SITE_NAME'); ?>/</strong></a> </div>
                                     <input class="form-control" type="text" name="u" id="uv" placeholder="URL you want to read without giving a pageview" value="<?php if ($articlePermalinkURL) { echo $articlePermalinkURL;} ?>" >
