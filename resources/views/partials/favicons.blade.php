9e675f0601bc33c6df7caec67cac346793cab732
Oskar Joelson <oskar@joelson.org>
2020-01-31 21:08:33 +0100

    Include favicons

---
+++ resources/views/partials/favicons.blade.php
@@ -0,0 +1,21 @@
+
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="167x167" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-167x167.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-180x180.png">
+    <link rel="apple-touch-icon" sizes="1024x1024" href="{{ ROOT_URL }}/assets/images/favicons/apple-touch-icon-1024x1024.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ ROOT_URL }}/assets/images/favicons/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ ROOT_URL }}/assets/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="228x228" href="{{ ROOT_URL }}/assets/images/favicons/coast-228x228.png">
+    {{--     <link rel="manifest" href="{{ ROOT_URL }}/assets/images/favicons/manifest.json"> --}}
+    <link rel="shortcut icon" href="{{ ROOT_URL }}/assets/images/favicons/favicon.ico">
+    {{--    <link rel="yandex-tableau-widget" href="{{ ROOT_URL }}/assets/images/favicons/yandex-browser-manifest.json"> --}}
+    <meta name="msapplication-TileColor" content="#fff">
+    <meta name="msapplication-TileImage" content="{{ ROOT_URL }}/assets/images/favicons/mstile-144x144.png">
+    {{--    <meta name="msapplication-config" content="{{ ROOT_URL }}/assets/images/favicons/browserconfig.xml"> --}}
