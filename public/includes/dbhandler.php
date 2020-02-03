c7204f9494789c9ab3ffb27ef198b26fdbfa38df
Oskar Joelson <oskar@joelson.org>
2020-02-03 08:24:10 +0100

    Fix sql create statement

--- public/includes/dbhandler.php
+++ public/includes/dbhandler.php
@@ -6,44 +6,43 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 class DBHandler {
 
-	protected $pdo;
+    protected $pdo;
 
-	function __construct() {
-		$this->pdo = new PDO('mysql:host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME') . ';charset=utf8', env('DB_USER'), env('DB_PASSWORD'));
+    function __construct() {
+        $this->pdo = new PDO('mysql:host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME') . ';charset=utf8', env('DB_USER'), env('DB_PASSWORD'));
 
         if (!$this->tableExists()) {
             $this->createTable();
         }
-	}
+    }
 
-	public function read($url): ?array
+    public function read($url): ?array
     {
-		$stmt = $this->pdo->prepare('SELECT title, body FROM cache WHERE url = :url LIMIT 1');
-		$stmt->execute(array('url' => $url));
-		foreach ($stmt as $row) {
+        $stmt = $this->pdo->prepare('SELECT title, body FROM cache WHERE url = :url LIMIT 1');
+        $stmt->execute(array('url' => $url));
+        foreach ($stmt as $row) {
             return [$row['title'], $row['body']];
-		}
+        }
 
         return null;
-	}
+    }
 
-	function cache($url, $title, $body){
-		$stmt = $this->pdo->prepare("INSERT INTO cache (url, title, body) VALUES (:url, :title, :body)");
-		$stmt->bindParam(':url', $url);
-		$stmt->bindParam(':title', $title);
-		$stmt->bindParam(':body', $body);
-		$stmt->execute();
-	}
+    function cache($url, $title, $body){
+        $stmt = $this->pdo->prepare("INSERT INTO cache (url, title, body) VALUES (:url, :title, :body)");
+        $stmt->bindParam(':url', $url);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':body', $body);
+        $stmt->execute();
+    }
 
     private function createTable()
     {
         $stmt = $this->pdo->prepare("CREATE TABLE IF NOT EXISTS `cache` (
-  `id` BIGINT(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` BIGINT(20) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `url` text COLLATE utf8_unicode_ci NOT NULL,
   `title` text COLLATE utf8_unicode_ci NOT NULL,
   `body` longtext COLLATE utf8_unicode_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  PRIMARY KEY (`id`)
 )");
         $stmt->execute();
     }
