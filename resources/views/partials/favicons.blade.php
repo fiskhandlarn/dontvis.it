d5594c4fdbed5350b416288aa9eaf391cf80a420
Oskar Joelson <oskar@joelson.org>
2020-02-03 11:56:03 +0100

    Restore manifests

--- resources/views/partials/favicons.blade.php
+++ resources/views/partials/favicons.blade.php
@@ -13,9 +13,9 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ ROOT_URL }}/assets/images/favicons/favicon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ ROOT_URL }}/assets/images/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="228x228" href="{{ ROOT_URL }}/assets/images/favicons/coast-228x228.png">
-    {{--     <link rel="manifest" href="{{ ROOT_URL }}/assets/images/favicons/manifest.json"> --}}
+    <link rel="manifest" href="{{ ROOT_URL }}/assets/images/favicons/manifest.json">
     <link rel="shortcut icon" href="{{ ROOT_URL }}/assets/images/favicons/favicon.ico">
-    {{--    <link rel="yandex-tableau-widget" href="{{ ROOT_URL }}/assets/images/favicons/yandex-browser-manifest.json"> --}}
+    <link rel="yandex-tableau-widget" href="{{ ROOT_URL }}/assets/images/favicons/yandex-browser-manifest.json">
     <meta name="msapplication-TileColor" content="#fff">
     <meta name="msapplication-TileImage" content="{{ ROOT_URL }}/assets/images/favicons/mstile-144x144.png">
-    {{--    <meta name="msapplication-config" content="{{ ROOT_URL }}/assets/images/favicons/browserconfig.xml"> --}}
+    <meta name="msapplication-config" content="{{ ROOT_URL }}/assets/images/favicons/browserconfig.xml">
