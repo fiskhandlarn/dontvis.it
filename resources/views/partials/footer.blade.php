4b628cb86ba46eb5943da1ae78ea15ccba38d3b8
Oskar Joelson <oskar@joelson.org>
2020-02-03 11:55:54 +0100

    Move GA to head

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -42,14 +42,5 @@
             });
         });
     </script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
-    </script>
   </body>
 </html>
