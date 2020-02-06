67decca7d7083f669bd6251b6da479a74ff99df7
Oskar Joelson <oskar@joelson.org>
2020-02-06 11:44:36 +0100

    Move parsing code to separate class

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Dontvisit\Parser;
 use eftec\bladeone\BladeOne;
-use Readability\Readability;
 
 if (!ob_start("ob_gzhandler")) {
     //gzip-e-di-doo-da
@@ -99,229 +99,14 @@ if ($hasURL) {
         //$UAstring = "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)\r\n";
         //$UAstring = "Baiduspider+(+http://www.baidu.com/search/spider.htm)  \r\n";
 
-        // Create a stream
-        $opts = array(
-            'http'=>array(
-                'method'=>"GET",
-                'header'=>$UAstring
-            )
-        );
+        $p = new Parser($url);
+        if ($p->fetch($UAstring)) {
+            $p->parse();
+            if ($p->readabilitify()) {
+                $p->prettify();
 
-        $context = stream_context_create($opts);
-        $html = @file_get_contents($url, false, $context);
-
-        if ($html) {
-            require_once 'includes/Readability.php';
-            require_once 'includes/JSLikeHTMLElement.php';
-
-            // PHP Readability works with UTF-8 encoded content.
-            // If $html is not UTF-8 encoded, use iconv() or
-            // mb_convert_encoding() to convert to UTF-8.
-
-            // If we've got Tidy, let's clean up input.
-            // This step is highly recommended - PHP's default HTML parser
-            // often does a terrible job and results in strange output.
-            if (function_exists('tidy_parse_string')) {
-                $tidy = tidy_parse_string($html, [], 'UTF8');
-                $tidy->cleanRepair();
-                $html = $tidy->value;
-            }
-
-            // remove all attributes except some
-            // https://stackoverflow.com/a/3026411/1109380
-            $dom = new DOMDocument;
-            $dom->loadHTML($html);
-            $xpath = new DOMXPath($dom);
-            $nodes = $xpath->query('//@*');
-            foreach ($nodes as $node) {
-                if (!in_array($node->nodeName, ['xlink:href', 'rel', 'src', 'srcset', 'srcSet', 'sizes', 'href', 'media', 'sizes', 'value', 'content', 'dir', 'lang', 'xml:lang'])) {
-                    $node->parentNode->removeAttribute($node->nodeName);
-                }
-            }
-            $html = $dom->saveHTML();
-
-            // give it to Readability
-            $readability = new Readability($html, $url);
-
-            // print debug output?
-            // useful to compare against Arc90's original JS version -
-            // simply click the bookmarklet with FireBug's
-            // console window open
-            $readability->debug = false;
-
-            // convert links to footnotes?
-            //$readability->convertLinksToFootnotes = true;
-
-            // process it
-            $result = $readability->init();
-
-            // does it look like we found what we wanted?
-            if ($result) {
-                $title = $readability->getTitle()->textContent;
-
-                $content = $readability->getContent()->innerHTML;
-
-                // if we've got Tidy, let's clean it up for output
-                if (function_exists('tidy_parse_string')) {
-                    $tidy = tidy_parse_string($content,
-                                              ['indent'=>true, 'show-body-only'=>true],
-                                              'UTF8');
-                    $tidy->cleanRepair();
-                    $content = $tidy->value;
-                }
-
-                // strip potentially harmful tags
-                $content = strip_tags($content, join('', array(
-                    "<a>",
-                    "<abbr>",
-                    "<address>",
-                    "<area>",
-                    "<article>",
-                    "<aside>",
-                    "<audio>",
-                    "<b>",
-                    "<base>",
-                    "<bdi>",
-                    "<bdo>",
-                    "<blockquote>",
-                    "<body>",
-                    "<br>",
-                    "<button>",
-                    "<canvas>",
-                    "<caption>",
-                    "<cite>",
-                    "<code>",
-                    "<col>",
-                    "<colgroup>",
-                    "<command>",
-                    "<data>",
-                    "<datalist>",
-                    "<dd>",
-                    "<del>",
-                    "<details>",
-                    "<dfn>",
-                    "<div>",
-                    "<dl>",
-                    "<dt>",
-                    "<em>",
-                    "<embed>",
-                    "<fieldset>",
-                    "<figcaption>",
-                    "<figure>",
-                    "<footer>",
-                    "<form>",
-                    "<h1>",
-                    "<h2>",
-                    "<h3>",
-                    "<h4>",
-                    "<h5>",
-                    "<h6>",
-                    "<head>",
-                    "<header>",
-                    "<hgroup>",
-                    "<hr>",
-                    "<html>",
-                    "<i>",
-                    "<iframe>",
-                    "<img>",
-                    "<input>",
-                    "<ins>",
-                    "<kbd>",
-                    "<keygen>",
-                    "<label>",
-                    "<legend>",
-                    "<li>",
-                    "<link>",
-                    "<map>",
-                    "<mark>",
-                    "<math>",
-                    "<menu>",
-                    "<meta>",
-                    "<meter>",
-                    "<nav>",
-                    "<noscript>",
-                    "<object>",
-                    "<ol>",
-                    "<optgroup>",
-                    "<option>",
-                    "<output>",
-                    "<p>",
-                    "<p>",
-                    "<param>",
-                    "<pre>",
-                    "<progress>",
-                    "<q>",
-                    "<rp>",
-                    "<rt>",
-                    "<ruby>",
-                    "<s>",
-                    "<samp>",
-                    "<script>",
-                    "<section>",
-                    "<select>",
-                    "<small>",
-                    "<source>",
-                    "<span>",
-                    "<strong>",
-                    "<style>",
-                    "<sub>",
-                    "<summary>",
-                    "<sup>",
-                    "<table>",
-                    "<tbody>",
-                    "<td>",
-                    "<textarea>",
-                    "<tfoot>",
-                    "<th>",
-                    "<thead>",
-                    "<time>",
-                    "<title>",
-                    "<tr>",
-                    "<track>",
-                    "<u>",
-                    "<ul>",
-                    "<var>",
-                    "<video>",
-                    "<wbr>",
-                )));
-
-                // make all relative urls absolute
-                // https://stackoverflow.com/a/48837947/1109380
-                $urlParts = parse_url($url);
-                $domain = $urlParts['scheme'] . '://' . $urlParts['host'];
-                $tagsAndAttributes = [
-                    //'img' => 'src',
-                    'form' => 'action',
-                    'a' => 'href'
-                ];
-
-                $dom = new DOMDocument;
-                libxml_use_internal_errors(true); // https://stackoverflow.com/a/6090728/1109380
-                $dom->loadHTML(
-                    mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), // https://stackoverflow.com/a/8218649/1109380
-                    LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD
-                );
-                libxml_clear_errors();
-
-                $xpath = new DOMXPath($dom);
-
-                // first, prepend all urls with source domain
-                foreach ($tagsAndAttributes as $tag => $attr) {
-                    foreach ($xpath->query("//{$tag}[not(starts-with(@{$attr}, '//')) and not(starts-with(@{$attr}, 'http')) and not(starts-with(@{$attr}, '#'))]") as $node) {
-                        $node->setAttribute($attr, $domain . $node->getAttribute($attr));
-                    }
-                }
-
-                // second, prepend all urls with anonymizer
-                foreach ($tagsAndAttributes as $tag => $attr) {
-                    foreach ($xpath->query("//{$tag}[not(starts-with(@{$attr}, '#'))]") as $node) {
-                        $node->setAttribute($attr, 'http://nullrefer.com/?' . $node->getAttribute($attr));
-                    }
-                }
-
-                $content = $dom->saveHTML();
-
-                $body = $content;
+                $title = $p->title;
+                $body = $p->body;
 
                 // save to db
                 $db->cache($articlePermalinkURL, $title, $body);
@@ -333,9 +118,6 @@ if ($hasURL) {
             }
         }
     }
-    // else {
-    //     var_dump("From cache:");
-    // }
 
     if ($title && $body) {
         echo $blade->run("article",compact("title", "body", "url", "articlePermalinkURL", "permalink", "permalinkWithoutScheme"));
---
+++ src/Dontvisit/Parser.php
@@ -0,0 +1,367 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dontvisit;
+
+use Readability\Readability;
+use \DOMDocument;
+use \DOMXPath;
+use \Error;
+
+class Parser
+{
+    private $content = false;
+    private $html = false;
+    private $title = false;
+    private $url = false;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function __get($property) {
+        switch ($property) {
+            case "title":
+                return $this->title;
+            case "body":
+                return $this->content;
+            default:
+                break;
+        }
+    }
+
+    /*********************************************************************************
+     *    ___       __   ___                  __  __           __
+     *   / _ \__ __/ /  / (_)___   __ _  ___ / /_/ /  ___  ___/ /__
+     *  / ___/ // / _ \/ / / __/  /  ' \/ -_) __/ _ \/ _ \/ _  (_-<
+     * /_/   \_,_/_.__/_/_/\__/  /_/_/_/\__/\__/_//_/\___/\_,_/___/
+     *
+     *********************************************************************************/
+
+    public function fetch(string $UAString): bool
+    {
+        if (!$this->url) {
+            throw new Error('URL not set.');
+            return false;
+        }
+
+        // Create a stream
+        $opts = [
+            'http'=> [
+                'method' => 'GET',
+                'header' => $UAString,
+            ],
+        ];
+
+        $context = stream_context_create($opts);
+        $this->html = @file_get_contents($this->url, false, $context);
+
+        return !!$this->html;
+    }
+
+    public function parse()
+    {
+        if (!$this->url) {
+            throw new Error('URL not set.');
+            return false;
+        }
+
+        if (!$this->html) {
+            throw new Error('HTML not set.');
+            return false;
+        }
+
+        $html = $this->html;
+
+        // PHP Readability works with UTF-8 encoded content.
+        // If $html is not UTF-8 encoded, use iconv() or
+        // mb_convert_encoding() to convert to UTF-8.
+        if (mb_detect_encoding($html) !== 'UTF-8') {
+            $html = mb_convert_encoding($html, "UTF-8");
+        }
+
+        $html = $this->tidyClean($html);
+
+        $html = $this->removeAttributes($html);
+
+        $this->html = $html;
+    }
+
+    public function prettify()
+    {
+        if (!$this->url) {
+            throw new Error('URL not set.');
+            return false;
+        }
+
+        if (!$this->content) {
+            throw new Error('Content not set.');
+            return false;
+        }
+
+        $content = $this->content;
+
+        $content = $this->tidyIndent($content);
+        $content = $this->stripTags($content);
+        $content = $this->makeAbsoluteURLs($content, $this->url);
+
+        $this->content = $content;
+    }
+
+    public function readabilitify(): bool
+    {
+        if (!$this->url) {
+            throw new Error('URL not set.');
+            return false;
+        }
+
+        if (!$this->html) {
+            throw new Error('HTML not set.');
+            return false;
+        }
+
+        // give it to Readability
+        $readability = new Readability($this->html, $this->url);
+
+        // print debug output?
+        // useful to compare against Arc90's original JS version -
+        // simply click the bookmarklet with FireBug's
+        // console window open
+        $readability->debug = false;
+
+        // convert links to footnotes?
+        //$readability->convertLinksToFootnotes = true;
+
+        // process it
+        $result = $readability->init();
+
+        // does it look like we found what we wanted?
+        if ($result) {
+            $this->title = $readability->getTitle()->textContent;
+            $this->content = $readability->getContent()->innerHTML;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /*********************************************************************************
+     *    ___      _           __                   __  __           __
+     *   / _ \____(_)  _____ _/ /____    __ _  ___ / /_/ /  ___  ___/ /__
+     *  / ___/ __/ / |/ / _ `/ __/ -_)  /  ' \/ -_) __/ _ \/ _ \/ _  (_-<
+     * /_/  /_/ /_/|___/\_,_/\__/\__/  /_/_/_/\__/\__/_//_/\___/\_,_/___/
+     *
+     *********************************************************************************/
+
+    private function makeAbsoluteURLs(string $content, string $url): string
+    {
+        // determine the base url
+        $urlParts = parse_url($url);
+        $domain = $urlParts['scheme'] . '://' . $urlParts['host'];
+        $tagsAndAttributes = [
+            //'img' => 'src',
+            'form' => 'action',
+            'a' => 'href'
+        ];
+
+        // make all relative urls absolute
+        // https://stackoverflow.com/a/48837947/1109380
+        $dom = new DOMDocument;
+        libxml_use_internal_errors(true); // https://stackoverflow.com/a/6090728/1109380
+        $dom->loadHTML(
+            mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), // https://stackoverflow.com/a/8218649/1109380
+            LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD
+        );
+        libxml_clear_errors();
+
+        $xpath = new DOMXPath($dom);
+
+        // first, prepend all urls with source domain
+        foreach ($tagsAndAttributes as $tag => $attr) {
+            foreach ($xpath->query("//{$tag}[not(starts-with(@{$attr}, '//')) and not(starts-with(@{$attr}, 'http')) and not(starts-with(@{$attr}, '#'))]") as $node) {
+                $node->setAttribute($attr, $domain . $node->getAttribute($attr));
+            }
+        }
+
+        // second, prepend all urls with anonymizer
+        foreach ($tagsAndAttributes as $tag => $attr) {
+            foreach ($xpath->query("//{$tag}[not(starts-with(@{$attr}, '#'))]") as $node) {
+                $node->setAttribute($attr, 'http://nullrefer.com/?' . $node->getAttribute($attr));
+            }
+        }
+
+        return $dom->saveHTML();
+    }
+
+    private function removeAttributes(string $html): string
+    {
+        // remove all attributes except some
+        // https://stackoverflow.com/a/3026411/1109380
+        $dom = new DOMDocument;
+        libxml_use_internal_errors(true); // https://stackoverflow.com/a/6090728/1109380
+        $dom->loadHTML($html);
+        libxml_clear_errors();
+        $xpath = new DOMXPath($dom);
+        $nodes = $xpath->query('//@*');
+        foreach ($nodes as $node) {
+            if (!in_array($node->nodeName, ['xlink:href', 'rel', 'src', 'srcset', 'srcSet', 'sizes', 'href', 'media', 'sizes', 'value', 'content', 'dir', 'lang', 'xml:lang'])) {
+                $node->parentNode->removeAttribute($node->nodeName);
+            }
+        }
+        $html = $dom->saveHTML();
+
+        return $html;
+    }
+
+    private function tidyClean(string $html): string
+    {
+        // If we've got Tidy, let's clean up input.
+        // This step is highly recommended - PHP's default HTML parser
+        // often does a terrible job and results in strange output.
+        if (function_exists('tidy_parse_string')) {
+            $tidy = tidy_parse_string($html, [], 'UTF8');
+            $tidy->cleanRepair();
+            $html = $tidy->value;
+        }
+
+        return $html;
+    }
+
+    private function stripTags(string $content): string
+    {
+        // strip potentially harmful tags
+        return strip_tags(
+            $content,
+            join('', [
+                '<a>',
+                '<abbr>',
+                '<address>',
+                '<area>',
+                '<article>',
+                '<aside>',
+                '<audio>',
+                '<b>',
+                '<base>',
+                '<bdi>',
+                '<bdo>',
+                '<blockquote>',
+                '<body>',
+                '<br>',
+                '<button>',
+                '<canvas>',
+                '<caption>',
+                '<cite>',
+                '<code>',
+                '<col>',
+                '<colgroup>',
+                '<command>',
+                '<data>',
+                '<datalist>',
+                '<dd>',
+                '<del>',
+                '<details>',
+                '<dfn>',
+                '<div>',
+                '<dl>',
+                '<dt>',
+                '<em>',
+                '<embed>',
+                '<fieldset>',
+                '<figcaption>',
+                '<figure>',
+                '<footer>',
+                '<form>',
+                '<h1>',
+                '<h2>',
+                '<h3>',
+                '<h4>',
+                '<h5>',
+                '<h6>',
+                '<head>',
+                '<header>',
+                '<hgroup>',
+                '<hr>',
+                '<html>',
+                '<i>',
+                '<iframe>',
+                '<img>',
+                '<input>',
+                '<ins>',
+                '<kbd>',
+                '<keygen>',
+                '<label>',
+                '<legend>',
+                '<li>',
+                '<link>',
+                '<map>',
+                '<mark>',
+                '<math>',
+                '<menu>',
+                '<meta>',
+                '<meter>',
+                '<nav>',
+                '<noscript>',
+                '<object>',
+                '<ol>',
+                '<optgroup>',
+                '<option>',
+                '<output>',
+                '<p>',
+                '<p>',
+                '<param>',
+                '<pre>',
+                '<progress>',
+                '<q>',
+                '<rp>',
+                '<rt>',
+                '<ruby>',
+                '<s>',
+                '<samp>',
+                '<script>',
+                '<section>',
+                '<select>',
+                '<small>',
+                '<source>',
+                '<span>',
+                '<strong>',
+                '<style>',
+                '<sub>',
+                '<summary>',
+                '<sup>',
+                '<table>',
+                '<tbody>',
+                '<td>',
+                '<textarea>',
+                '<tfoot>',
+                '<th>',
+                '<thead>',
+                '<time>',
+                '<title>',
+                '<tr>',
+                '<track>',
+                '<u>',
+                '<ul>',
+                '<var>',
+                '<video>',
+                '<wbr>',
+            ])
+        );
+    }
+
+    private function tidyIndent(string $content): string
+    {
+        // if we've got Tidy, let's clean it up for output
+        if (function_exists('tidy_parse_string')) {
+            $tidy = tidy_parse_string($content,
+                                      ['indent'=>true, 'show-body-only'=>true],
+                                      'UTF8');
+            $tidy->cleanRepair();
+            $content = $tidy->value;
+        }
+
+        return $content;
+    }
+}
