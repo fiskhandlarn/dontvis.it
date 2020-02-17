3c6f61179afe7905a2b8409f61a412e1e81961dd
Oskar Joelson <oskar@joelson.org>
2020-02-17 13:10:38 +0100

    Check all variables before using them for og tags

--- resources/views/partials/og.blade.php
+++ resources/views/partials/og.blade.php
@@ -1,12 +1,18 @@
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="{{ env('SITE_NAME') }}" />
     <meta property="og:image" content="{{ ROOT_URL }}/assets/images/og.png" />
-@if (isset($title))
+@if (isset($permalink))
     <meta property="og:url" content="{{ $permalink }}"/>
-    <meta property="og:title" content="{{ $title }} &ndash; {{ env('SITE_NAME') }}" />
-    <meta property="og:description" content="{{ $excerpt }}" />
 @else
     <meta property="og:url" content="{{ ROOT_URL }}/"/>
+@endif
+@if (isset($title))
+    <meta property="og:title" content="{{ $title }} &ndash; {{ env('SITE_NAME') }}" />
+@else
     <meta property="og:title" content="{{ env('SITE_NAME') }} &ndash; avoid endorsing idiots" />
+@endif
+@if (isset($excerpt))
+    <meta property="og:description" content="{{ $excerpt }}" />
+@else
     <meta property="og:description" content="{{ env('SITE_NAME') }} is a tool to escape linkbaits, trolls, idiots and asshats." />
 @endif
