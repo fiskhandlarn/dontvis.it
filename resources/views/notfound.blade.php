fb89adc42b7d7e8e401888378d6a5a09624f86b4
Oskar Joelson <oskar@joelson.org>
2020-02-17 14:16:03 +0100

    Anonymize url on not found page

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -3,6 +3,6 @@
 @section('main')
 
     <div class="container not-found">
-        <p>Looks like we couldn't find any content at <a href="{{ $url }}">{{ $url }}</a>.</p>
+        <p>Looks like we couldn't find any content at <a href="{{ env('ANONYMIZER_URL') }}{{ $url }}">{{ $url }}</a>.</p>
     </div>
 @stop
