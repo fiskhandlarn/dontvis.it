1317caf3535bd3ae0712ab6cf82bc214758e9a9d
Oskar Joelson <oskar@joelson.org>
2020-02-17 13:12:57 +0100

    Restore accidental commit from 98f35224414aa167df42562f7d9ae8da1348374d

--- public/index.php
+++ public/index.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 use Dontvisit\Parser;
 use eftec\bladeone\BladeOne;
 use Illuminate\Support\Str;
