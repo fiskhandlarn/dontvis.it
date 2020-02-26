a7ec9a55601d6fee0e9a9bffe43aedbb1c615c82
Oskar Joelson <oskar@joelson.org>
2020-02-26 13:06:33 +0100

    Use IP numbers for testing, test for no redirection, test scraped title, test same response code for both localhost and docker

--- tests/URLTest.php
+++ tests/URLTest.php
@@ -4,20 +4,19 @@ namespace Dontvisit;
 
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Client;
+use DOMDocument;
 
 class URLTest extends TestCase
 {
     private static $client;
     private static $host;
-    private static $isDocker = false;
 
     public static function setUpBeforeClass(): void
     {
         if (file_exists('/.dockerenv')) {
-            self::$host = 'https://host.docker.internal:3000/';
-            self::$isDocker = true;
+            self::$host = 'https://172.17.0.1:3000/';
         } else {
-            self::$host = 'https://localhost:3000/';
+            self::$host = 'https://127.0.0.1:3000/';
         }
 
         echo "Using " . self::$host . "\n";
@@ -34,19 +33,21 @@ class URLTest extends TestCase
         $response = self::$client->request('GET', '');
         $this->assertEquals(200, $response->getStatusCode());
 
-        $response = self::$client->request('GET', urlencode('github.com/fiskhandlarn/dontvis.it'), ['allow_redirects' => false]);
+        // test for no redirection
+        $response = self::$client->request('GET', urlencode('github.com/fiskhandlarn/dontvis.it'));
         $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testScrape()
+    {
+        $response = self::$client->request('GET', urlencode('github.com/fiskhandlarn/dontvis.it'));
+        $this->assertEquals('fiskhandlarn/dontvis.it: dontvis.it, the idiot circumventor tool – dontvis.it', $this->getTitle($response->getBody()->getContents()));
     }
 
     public function test301()
     {
         $response = self::$client->request('GET', urlencode(self::$host), ['allow_redirects' => false]);
-        if (self::$isDocker) {
-            $this->assertEquals(301, $response->getStatusCode());
-        } else {
-            // localhost doesn't have a tld, we will get a 404 instead of 301
-            $this->assertEquals(404, $response->getStatusCode());
-        }
+        $this->assertEquals(301, $response->getStatusCode());
     }
 
     public function test303()
@@ -59,5 +60,23 @@ class URLTest extends TestCase
     {
         $response = self::$client->request('GET', 'abc');
         $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    // https://stackoverflow.com/a/30523600/1109380
+    private function getTitle($html): string
+    {
+        $title = '';
+        $dom = new DOMDocument();
+
+        libxml_use_internal_errors(true); // https://stackoverflow.com/a/6090728/1109380
+        if ($dom->loadHTML($html)) {
+            $list = $dom->getElementsByTagName("title");
+            if ($list->length > 0) {
+                $title = $list->item(0)->textContent;
+            }
+        }
+        libxml_clear_errors();
+
+        return $title;
     }
 }
